3f5514716f8acc37c507defa7f7bb5d6cd5be055
hanjie1 <liu28238@gmail.com>
2022-08-04 16:16:08 -0400

    analysis scripts

--- rd_demo.py
+++ rd_demo.py
@@ -38,8 +38,9 @@ def data_valid (raw):
 #fp =  "D:/debug_data/Raw_MULTCONFIG_03_08_2022.bin"
 
 
-for i in range(0,64,4):
-    fp =  "D:/debug_data/Raw_CALI_200mVBL_14_0mVfC_2_0us_0x{:02x}_03_08_2022.bin".format(i)
+for i in range(1):
+    #fp =  "/home/hanjie/Desktop/protoDUNE/cold_electronics/FEMB_QC/new_qc_data/data/Raw_CALI_200mVBL_14_0mVfC_2_0us_0x{:02x}_03_08_2022.bin".format(i)
+    fp =  "/home/hanjie/Desktop/protoDUNE/cold_electronics/FEMB_QC/new_qc_data/data/Raw_MULTCONFIG_03_08_2022.bin"
     
     with open(fp, 'rb') as fn:
         raw = pickle.load(fn)
