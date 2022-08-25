03d1e8163615c9a04a08b46f8b47b16d11dd6669
Hanjie Liu <hanjie@Hanjies-MacBook-Air.local>
2022-08-24 20:36:58 -0400

    add two more functions

--- backup.py
+++ backup.py
@@ -341,67 +341,6 @@ class QC_Runs:
         with open(fp, 'wb') as fn:
             pickle.dump( [rawdata, pwr_meas, cfg_paras_rec, logs], fn)
 
-    def femb_leakage_cur(self):
-
-        datadir = self.save_dir+"Leak_Current/"
-        try:
-            os.makedirs(datadir)
-        except OSError:
-            print ("Error to create folder %s !!! Continue to next test........"%datadir)
-            return 
-        
-        snc = 1 # 200 mV
-        sg0 = 0
-        sg1 = 0 # 14mV/fC
-        st0 = 1
-        st1 = 1 # 2us 
-        
-        
-        chk.femb_cd_rst()
-        
-        sncs = self.sncs
-        sgs = self.sgs
-        sts = "2_0us"
-
-        st0=1 
-        st1=1 # 2us
-
-        for idac in range(0,64,4):  # dac 
-            for snci in range(2):
-                for sgi in range(4):
-                    sg0=sgi//2
-                    sg1=sgi%2
-
-                    cfg_paras_rec = []
-                    chk.femb_cd_rst()
-                    chk.femb_cd_sync()
-                    time.sleep(1)
-                    for femb_id in fembs:
-                        chk.adcs_paras = [ # c_id, data_fmt(0x89), diff_en(0x84), sdc_en(0x80), vrefp, vrefn, vcmo, vcmi, autocali
-                                           [0x4, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
-                                           [0x5, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
-                                           [0x6, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
-                                           [0x7, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
-                                           [0x8, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
-                                           [0x9, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
-                                           [0xA, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
-                                           [0xB, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
-                                         ]
-                        chk.set_fe_board(sts=1, snc=snci, sg0=sg0, sg1=sg1, st0=st0, st1=st1, swdac=1, dac=idac )
-                        adac_pls_en = 1 # enable LArASIC interal calibraiton pulser
-                        cfg_paras_rec.append( (femb_id, copy.deepcopy(chk.adcs_paras), copy.deepcopy(chk.regs_int8), adac_pls_en) )
-                        chk.femb_cfg(femb_id, adac_pls_en )
-        
-                    time.sleep(0.5)
-                    pwr_meas = chk.get_sensors()
-                
-                    rawdata = chk.wib_acquire_data(fembs=fembs, num_samples=sample_N) #returns lsti of size 1
-                    if save:
-                        fdir = self.save_dir
-                        fp = fdir + "Raw_CALI_SE_{}_{}_{}_0x{:02x}".format(sncs[snci],sgs[sgi],sts,idac)  + ".bin"
-                        with open(fp, 'wb') as fn:
-                            pickle.dump( [rawdata, pwr_meas, cfg_paras_rec, self.logs], fn)
-
     def femb_rms(self):
 
         fembs = self.fembs
@@ -554,6 +493,75 @@ class QC_Runs:
                         with open(fp, 'wb') as fn:
                             pickle.dump( [rawdata, pwr_meas, cfg_paras_rec, self.logs], fn)
         
+
+    def femb_one_config(self, femb_id, chk, snci, sgi, sti, dac, sdd=0, sdf=0):
+
+        sncs = self.sncs 
+        sgs = self.sgs
+        sts = self.sts
+
+        snc = sncs.index(snci)
+        sg0 = sgs.index(sgi)//2
+        sg1 = sgs.index(sgi)%2
+        st0 = sts.index(sti)//2
+        st1 = sts.index(sti)%2
+
+        cfg_paras_rec = []
+        chk.adcs_paras = [ # c_id, data_fmt(0x89), diff_en(0x84), sdc_en(0x80), vrefp, vrefn, vcmo, vcmi, autocali
+                           [0x4, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
+                           [0x5, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
+                           [0x6, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
+                           [0x7, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
+                           [0x8, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
+                           [0x9, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
+                           [0xA, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
+                           [0xB, 0x08, 0, 0, 0xDF, 0x33, 0x89, 0x67, 1],
+                         ]
+
+        if sdd==1:
+            for i in range(8):
+                chk.adcs_paras[i][2]=1   # enable differential 
+
+        chk.set_fe_board(sts=1, snc=snc, sg0=sg0, sg1=sg1, st0=st0, st1=st1, swdac=1, dac=dac, sdd=sdd, sdf=sdf)
+        adac_pls_en = 1 # enable LArASIC interal calibraiton pulser
+        cfg_paras_rec.append( (femb_id, copy.deepcopy(chk.adcs_paras), copy.deepcopy(chk.regs_int8), adac_pls_en) )
+        chk.femb_cfg(femb_id, adac_pls_en )
+        return cfg_paras_rec
+        
+
+    def femb_difconfig(self):
+        fembs = self.fembs
+        sample_N = self.sample_N
+        chk = WIB_CFGS()
+        
+        chk.wib_init()
+        
+        chk.femb_vol_set(vfe=3.0, vcd=3.0, vadc=3.5)
+        chk.femb_powering(fembs)
+        
+        chk.femb_cd_rst()
+        cfg_paras_rec = []
+
+        cfg_paras = self.femb_one_config(0, chk, '200mVBL', '14_0mVfC', '2_0us', 0x20, 0, 0)
+        cfg_paras_rec.append(cfg_paras)
+
+        cfg_paras = self.femb_one_config(1, chk, '900mVBL', '25_0mVfC', '1_0us', 0x10, 0, 0)
+        cfg_paras_rec.append(cfg_paras)
+
+        cfg_paras = self.femb_one_config(2, chk, '200mVBL', '14_0mVfC', '2_0us', 0x20, 1, 0)
+        cfg_paras_rec.append(cfg_paras)
+
+        cfg_paras = self.femb_one_config(3, chk, '900mVBL', '14_0mVfC', '2_0us', 0x20, 0, 1)
+        cfg_paras_rec.append(cfg_paras)
+
+        time.sleep(0.5)
+        pwr_meas = chk.get_sensors()
+        rawdata = chk.wib_acquire_data(fembs=fembs, num_samples=sample_N) #returns lsti of size 1
+        if save:
+           fdir = self.save_dir
+           fp = fdir + "Raw_MULTCONFIG_"  + ".bin"
+           with open(fp, 'wb') as fn:
+                pickle.dump( [rawdata, pwr_meas, cfg_paras_rec, self.logs], fn)
 
 if __name__=='__main__':
                         
