e9cd1a97cd9917885bc2f558aadada17a180b57c
hanjie1 <liu28238@gmail.com>
2022-11-23 15:34:31 +0100

    new CRU map and analysis script

---
+++ analysis/cru_rms.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pickle
+from tools import Tools
+import matplotlib.pyplot as plt
+
+
+fp ="../data/Raw_SW_Trig19_11_2022_22_48_23.bin"
+with open(fp, 'rb') as fn:
+    raw = pickle.load(fn)
+
+rawdata = raw[0]
+pwr_meas = raw[1]
+print(len(rawdata))
+
+tl=Tools()
+ch_data = tl.data_ana(rawdata)   # return data is a numpy array, axis=0: event No., axis=1: Chan No, axis=2: ADC bit
+ch_rms = np.std(ch_data, axis =(0,2) )  # rms for each channel
+
+#uplane=[]*476
+#vplane=[]*476
+#xplane=[]*584
+uplane = np.zeros(476)
+vplane = np.zeros(476)
+xplane = np.zeros(584)
+
+for i in range(1536):
+    nfemb = i//128 + 1
+    nch = i %128
+
+    dfmap = tl.LoadMap(nfemb)
+    plane,strip = tl.FindStrips(dfmap, nfemb, nch)
+    #print(i, plane, strip)
+
+    if plane==1:
+       uplane[strip-1]=ch_rms[i]
+    if plane==2:
+       vplane[strip-1]=ch_rms[i]
+    if plane==3:
+       xplane[strip-1]=ch_rms[i]
+
+xx=range(1536)
+plt.plot(xx, ch_rms)
+ch_rms_map = np.concatenate((uplane,vplane,xplane))
+plt.plot(xx, ch_rms_map)
+plt.show()
