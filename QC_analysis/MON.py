dccd4882d3ab0ea9ee8e46b270a222001c519e40
fanrado <radofana@gmail.com>
2022-11-29 12:10:27 -0500

    add script to monitor ColdADC

--- QC_analysis/MON.py
+++ QC_analysis/MON.py
@@ -15,11 +15,12 @@ class MON_LARASIC:
             output_dir: self.output_dir,
             fembs_to_exclude: a list of the femb_id to exclude
     '''
-    def __init__(self, input_dir='', output_dir='', temperature='LN', fembs_to_exclude=[]):
+    def __init__(self, input_dir='', output_dir='', output_dirname='MON_FE', temperature='LN', fembs_to_exclude=[]):
         print('..........init............')
         self.temperature = temperature
         self.data_dir = input_dir
-        self.output_dir = '/'.join([output_dir, 'MON_FE'])
+        self.output_dirname = output_dirname
+        self.output_dir = '/'.join([output_dir, self.output_dirname])
         try:
             os.mkdir(self.output_dir)
         except:
@@ -46,7 +47,7 @@ class MON_LARASIC:
         for femb_folder in os.listdir(self.data_dir):
             if (self.temperature in femb_folder) & (not self.exclude_fembs(fembs_to_exclude=fembs_to_exclude, fembs_folder_name=femb_folder)):
                 if 'MON_FE' in os.listdir('/'.join([self.data_dir, femb_folder])):
-                    femb_dir_list.append('/'.join([self.data_dir, femb_folder, 'MON_FE']))
+                    femb_dir_list.append('/'.join([self.data_dir, femb_folder, self.output_dirname]))
         return femb_dir_list
 
     def read_bin(self, bin_dir='', bin_filename='LArASIC_mon.bin'):
@@ -58,7 +59,7 @@ class MON_LARASIC:
         with open(path_to_bin, 'rb') as bin_pointer:
             return pickle.load(bin_pointer)
     
-    def get_data_index(self, dataname='bandgap'):
+    def __get_data_index(self, dataname='bandgap'):
         '''
             This function returns the index of dataname in the data_list.
         '''
@@ -75,7 +76,7 @@ class MON_LARASIC:
             index = 4
         return index
 
-    def get_MONdata(self, data_from_bin=[], dataname='bandgap'):
+    def __get_MONdata(self, data_from_bin=[], dataname='bandgap'):
         '''
             input:
                 data_from_bin: a list returned after reading the bin data,
@@ -84,12 +85,12 @@ class MON_LARASIC:
                 a list of the data for 4 fembs after calculating the average of the data saved.
                 For 200mVBL and 900mVBL, xx represents the channel numbers.
         '''
-        index_MONdata = self.get_data_index(dataname=dataname)
+        index_MONdata = self.__get_data_index(dataname=dataname)
         index_logs = -1
         if 'dac' in dataname:
-            index_logs = self.get_data_index(dataname='dac_logs')
+            index_logs = self.__get_data_index(dataname='dac_logs')
         else:
-            index_logs = self.get_data_index(dataname='logs')
+            index_logs = self.__get_data_index(dataname='logs')
         fadc = 1/(2**14) * 2048 # mV
         mon_refs = data_from_bin[index_MONdata]
         fembs = data_from_bin[index_logs]['femb id']
@@ -112,7 +113,7 @@ class MON_LARASIC:
             data_allFEMBs.append((xx, mon_list))
         return data_allFEMBs
         
-    def plotMon(self, femb_data_x, femb_data_y, femb_id, dataname='bandgap', xlabel='', ylabel='', output_dir=''):
+    def __plotMon(self, femb_data_x, femb_data_y, femb_id, dataname='bandgap', xlabel='', ylabel='', output_dir=''):
         '''
         Save monitoring data to png file.
         '''
@@ -126,7 +127,7 @@ class MON_LARASIC:
         plt.savefig('/'.join([output_dir, '_'.join(['mon', dataname, 'femb' + femb_id + '.png'])]))
         plt.close()
     
-    def saveMon_to_csv(self, femb_data_x, femb_data_y, femb_id, dataname='bandgap', output_dir=''):
+    def __saveMon_to_csv(self, femb_data_x, femb_data_y, femb_id, dataname='bandgap', output_dir=''):
         '''
             Save monitoring data to csv file.
         '''
@@ -137,7 +138,7 @@ class MON_LARASIC:
         csvname = '_'.join(['mon', dataname, 'femb' + femb_id])
         df.to_csv('/'.join([output_dir, csvname + '.csv']), index=False)
 
-    def get_dist(self, dataname, data=[], output_dir=''):
+    def __get_dist(self, dataname, data=[], output_dir=''):
         mean = np.mean(data)
         std = np.std(data)
         figname = '_'.join(['hist', dataname, self.temperature])
@@ -172,9 +173,9 @@ class MON_LARASIC:
             histdata_y = []
             for idir in range(len(self.femb_dir_list)):
                 data = self.read_bin(self.femb_dir_list[idir], bin_filename='LArASIC_mon.bin')
-                fembs = data[self.get_data_index(dataname='logs')]['femb id']
+                fembs = data[self.__get_data_index(dataname='logs')]['femb id']
 
-                MON_data = self.get_MONdata(data_from_bin=data, dataname=dataname)
+                MON_data = self.__get_MONdata(data_from_bin=data, dataname=dataname)
                 ylabel = dataname + '(mV)'
                 xlabel = 'chip#'
                 if 'BL' in dataname:
@@ -191,19 +192,19 @@ class MON_LARASIC:
                     except:
                         pass
                         # print('Folder already exists.....')
-                    self.plotMon(femb_data_x=MON_data[iifemb][0], femb_data_y=MON_data[iifemb][1],
+                    self.__plotMon(femb_data_x=MON_data[iifemb][0], femb_data_y=MON_data[iifemb][1],
                                 femb_id=femb_id, dataname=dataname,
                                 xlabel=xlabel, ylabel=ylabel, output_dir=new_output_dir)
-                    self.saveMon_to_csv(femb_data_x=MON_data[iifemb][0], femb_data_y=MON_data[iifemb][1],
+                    self.__saveMon_to_csv(femb_data_x=MON_data[iifemb][0], femb_data_y=MON_data[iifemb][1],
                                         femb_id=femb_id, dataname=dataname,
                                         output_dir=new_output_dir)
                     histdata_x += MON_data[iifemb][0]
                     histdata_y += MON_data[iifemb][1]
-            self.get_dist(dataname=dataname, data=histdata_y, output_dir=self.output_dir)
+            self.__get_dist(dataname=dataname, data=histdata_y, output_dir=self.output_dir)
         print('-------LArASIC_mon DONE------------')
 
 
-    def checkLinearity(self, x=[], y=[], lowdac=0, updac=30):
+    def checkLinearity(self, x=[], y=[], lowdac=0, updac=30, indexOfMax=0, indexOfMin=-1):
         '''
             Check the linearity of the plot for each chip.
             The slope for LArASIC_DAC is negative.
@@ -231,7 +232,7 @@ class MON_LARASIC:
         for i in range(old_index+1, len(x)):
             y_pred = slope * x[i] + y0
             # err = np.std(y[index_low_dac:old_index+1]) / len(y[index_low_dac:old_index+1])
-            condition = (np.abs(y_pred - y[i])/(y[0]-y[-1])) > 0.01
+            condition = (np.abs(y_pred - y[i])/(y[indexOfMax]-y[indexOfMin])) > 0.01
             if condition:
                 index = i
                 break
@@ -256,10 +257,11 @@ class MON_LARASIC:
         '''
         datanames = ['dac_sgp1', 'dac_14mVfC']
         for dataname in datanames:
+            maxLinearDAC_list = []
             for idir in range(len(self.femb_dir_list)):
                 data = self.read_bin(self.femb_dir_list[idir], bin_filename='LArASIC_mon_DAC.bin')
-                fembs = data[self.get_data_index(dataname='dac_logs')]['femb id']
-                MON_data = self.get_MONdata(data_from_bin=data, dataname=dataname)
+                fembs = data[self.__get_data_index(dataname='dac_logs')]['femb id']
+                MON_data = self.__get_MONdata(data_from_bin=data, dataname=dataname)
                 for ifemb, femb_id in fembs.items():
                     iifemb = int(ifemb[-1])
                     x = MON_data[iifemb][0]
@@ -298,6 +300,7 @@ class MON_LARASIC:
                         # plt.plot(x, y, label='fit', color='red')
                         print(lin)
                         print(slope)
+                    maxLinearDAC_list += maxLinearDAC
                     chipNumber = ['chip{}'.format(ichip) for ichip in tmpchipNumber]
                     maxLinearDAC_df = pd.DataFrame({'chip': chipNumber, 'maxDAC': maxLinearDAC})
                     
@@ -318,8 +321,93 @@ class MON_LARASIC:
                     plt.legend()
                     plt.savefig('/'.join([new_output_dir, figname + '.png']))
                     plt.close()
-        
+            meanDAC = np.mean(maxLinearDAC_list)
+            plt.figure(figsize=(10, 7))
+            plt.hist(maxLinearDAC_list, bins=100, label='mean = {}'.format(meanDAC))
+            plt.xlabel('max linear DAC')
+            plt.ylabel('#')
+            plt.title(dataname)
+            plt.legend()
+            plt.savefig('/'.join([self.output_dir, 'distribution_maxLinearDAC_{}.png'.format(dataname)]))
+            plt.close()
 
-class MON_ADC:
-    def __init__(self, input_dir=''):
-        pass
+class MON_ColdADC(MON_LARASIC):
+    '''
+        Monitoring ColdADC.
+    '''
+    def __init__(self, input_dir='', output_dir='', temperature='LN', fembs_to_exclude=[]):
+        MON_LARASIC.__init__(self, input_dir=input_dir, output_dir=output_dir, output_dirname='MON_ADC',
+                            temperature=temperature, fembs_to_exclude=fembs_to_exclude)
+        self.mons = ['VCMI', 'VCMO', 'VREFP', 'VREFN']
+
+    def __output_dir(self, data_from_bin=[], femb_id=''):
+        toytpc = data_from_bin[2]['toytpc']
+        femb_folderName = '_'.join(['FEMB', femb_id, self.temperature, toytpc])
+        new_output_dir = '/'.join([self.output_dir, femb_folderName])
+        try:
+            os.mkdir(new_output_dir)
+        except:
+            pass
+        return new_output_dir
+
+    def __get_MONADC_data(self, data_from_bin=[]):
+        '''
+            get MON_ADC data for 4 fembs after conversion to mV
+        '''
+        fadc = 1/(2**14) * 2048 # mV
+        mon_adc = data_from_bin[1]
+        fembs = data_from_bin[2]['femb id']
+        
+        for ifemb, femb_id in fembs.items():
+            nfemb = int(ifemb[-1])
+            for i in range(len(self.mons)):
+                ndac = len(mon_adc)
+                # mon_for_allchips = []
+                chips_list = []
+                mon_list = []
+                dac_list = []
+                for ichip in range(8):
+                    mon_values_list = []
+                    dac_values_list = []
+                    for k in range(ndac):
+                        value = mon_adc[k][1][1+i][f'chip{ichip}'][3]
+                        sps = len(value)
+                        total = list(map(sum, zip(*value)))
+                        avg = np.array(total)/sps * fadc
+                        mon_values_list.append(avg[nfemb])
+                        dac_values_list.append(mon_adc[k][0])
+
+                        chips_list.append(f'chip{ichip}')
+                        # mon_for_allchips.append(mons[i])
+                    mon_list += mon_values_list
+                    dac_list += dac_values_list
+                #
+                # iifemb = int(ifemb[-1])
+                new_output_dir = self.__output_dir(data_from_bin=data_from_bin, femb_id=femb_id)
+                tmp_df = pd.DataFrame({'CHIP': chips_list,
+                                        'DAC': dac_list,
+                                        'MON': mon_list})
+                tmp_df.to_csv('/'.join([new_output_dir, self.mons[i] + '.csv']), index=False)
+
+    def __plot_MonColdADC(self, mon_dataname='VCMI', femb_id='', data_from_bin=[]):
+        csv_dir = self.__output_dir(data_from_bin=data_from_bin, femb_id=femb_id)
+        df = pd.read_csv('/'.join([csv_dir, mon_dataname + '.csv']))
+        plt.figure(figsize=(10, 7))
+        for ichip in range(8):
+            tmp_df = df[df['CHIP'] == f'chip{ichip}']
+            plt.plot(tmp_df['DAC'], tmp_df['MON'], marker='.', markersize='5.5', label=f'chip{ichip}')
+        plt.xlabel('DAC')
+        plt.ylabel(mon_dataname+'(mV)')
+        plt.title(mon_dataname)
+        plt.legend()
+        plt.savefig('/'.join([csv_dir, mon_dataname + '.png']))
+        plt.close()
+
+    def run_MON_ColdADC(self):
+        for femb_folder in self.femb_dir_list:
+            raw = self.read_bin(bin_dir=femb_folder, bin_filename='LArASIC_ColdADC_mon.bin')
+            self.__get_MONADC_data(data_from_bin=raw)
+            fembs = raw[2]['femb id']
+            for ifemb, femb_id in fembs.items():
+                for mon in self.mons:
+                    self.__plot_MonColdADC(mon_dataname=mon, femb_id=femb_id, data_from_bin=raw)
