b6f46b60398e01e6f4216983a5ef9a2850f1d959
fanrado <radofana@gmail.com>
2022-12-08 20:08:39 -0500

    group script in functions

--- QC_analysis/run_analysis.py
+++ QC_analysis/run_analysis.py
@@ -7,14 +7,121 @@
 import os
 from MON import MON_LARASIC, MON_ColdADC
 # from QC_analysis import QC_analysis
-# from ASICDAC import savegains, Gains_CALI1, Gains_CALI2, Gains_CALI3_or_CALI4
-# from ASICDAC import get_ENC_CALI
+from ASICDAC import savegains, Gains_CALI1, Gains_CALI2, Gains_CALI3_or_CALI4
+from ASICDAC import get_ENC_CALI
 # from ASICDAC import save_peakValues_to_csv
-# from ASICDAC import separateCSV_foreachFEMB
-# from ASICDAC import distribution_ENC_Gain
+from ASICDAC import separateCSV_foreachFEMB
+from ASICDAC import distribution_ENC_Gain
 #
 # for RMS, Pedestal and PWR
-# from ASICDAC import removeFEMB_from_T, all_PWR_Meas_plots
+# from ASICDAC import all_PWR_Meas_plots
+from QC_analysis import QC_analysis, save_allInfo_PWR_tocsv, plot_PWR_Consumption, all_PWR_Meas_plots, save_allInfo_PWRCycle_tocsv, plot_PWR_Cycle
+import Analysis_FEMB_QC
+
+def run_PWR_Meas(inputdir, savedir):
+    # ------------------------------------------------------
+    measured_info = ['P_meas', 'V_meas', 'I_meas']
+    temperatures = ['LN', 'RT']
+    dataname_list = ['Bias5V', 'LArASIC', 'ColdDATA', 'ColdADC']
+    # -----------This is a group ---------------------------
+    # save data in csv file
+    save_allInfo_PWR_tocsv(data_input_dir=inputdir, output_dir=savedir, temperature_list=temperatures, dataname_list=dataname_list)
+    
+    # produce all the plots
+    all_PWR_Meas_plots(csv_source_dir=savedir, measured_info_list=measured_info, temperature_list=temperatures, dataname_list=dataname_list)
+    ### Get power consumption ------- PWR_Meas------------
+    plot_PWR_Consumption(csv_source_dir=savedir, temperatures=temperatures,
+                        all_data_types=dataname_list, output_dir=savedir, powerType='PWR_Meas')
+
+def run_RMS_Pedestal(inputdir, savedir, femb_to_exclude=[75, 24, 55, 7, 27]):
+    temperatures = ['LN', 'RT']
+    datanames = ['Pedestal', 'RMS']
+    #------Save RMS in csv files--------------------------
+    for T in temperatures:
+        qc = QC_analysis(datadir=inputdir, output_dir=savedir, temperature=T, dataType='RMS')
+        qc.save_rms_pedestal_to_csv()
+    #-----------END saving--------------------------------
+    # correct csv files for RMS by removing some fembs
+        path_to_csvfiles = '/'.join([savedir, T, 'RMS'])
+        output_path = '/'.join([savedir, T, 'RMS', 'correctedCSV_rms_pedestal'])
+        try:
+            os.mkdir(output_path)
+        except OSError:
+            print(OSError)
+
+        Analysis_FEMB_QC.saveCorrectedCSV(path_to_csv=path_to_csvfiles, femb_list=femb_to_exclude, output_path=output_path, datanames=datanames)
+
+        for dataname in datanames:
+            try:
+                os.mkdir('/'.join([output_path, 'plots']))
+            except OSError:
+                print(OSError)
+            sourceMainDir = output_path
+            new_output_path = '/'.join([output_path, 'plots', dataname])
+            try:
+                os.mkdir(new_output_path)
+            except OSError:
+                print(OSError)
+
+            Analysis_FEMB_QC.save_gaussianInfo(sourceMainDir=sourceMainDir,
+                                            dataType=dataname, outputDir=new_output_path,
+                                            nstd=3, nbins=60, skewed=True)
+            ylim200, ylim900 = [], []
+            if dataname=='Pedestal':
+                ylim200 = [500, 1650]
+                ylim900 = [7750, 9500]
+            elif dataname=='RMS':
+                ylim200 = [0, 55]
+                ylim900 = [0, 55]
+
+            Analysis_FEMB_QC.plot_mean_vs_ShapingTime(path_to_csv='/'.join([new_output_path, 'gaussian_'+dataname+'.csv']),
+                                                     BL='200mV',
+                                                     outputDir= new_output_path,
+                                                     ylabel=dataname,
+                                                     addToTitle='', skewed=True, ylim=ylim200)
+            Analysis_FEMB_QC.plot_mean_vs_ShapingTime(path_to_csv= new_output_path + '/gaussian_'+ dataname +'.csv',
+                                                     BL='900mV',
+                                                     outputDir=new_output_path,
+                                                     ylabel=dataname,
+                                                     addToTitle='', skewed=True, ylim=ylim900)
+
+def run_PWR_Cycle(inputdir, savedir):
+    measured_info = ['P_meas', 'V_meas', 'I_meas']
+    temperatures = ['LN', 'RT']
+    dataname_list = ['Bias5V', 'LArASIC', 'ColdDATA', 'ColdADC']
+    ## -----------------save PWR_Cycle to csv files-------
+    save_allInfo_PWRCycle_tocsv(data_input_dir='../data', output_dir='../data/analysis', temperature_list=temperatures, dataname_list=dataname_list)
+    #
+    # try to plot PWRCycle
+    for m_param in measured_info:
+       plot_PWR_Cycle(csv_source_dir='{}/LN/PWR_Cycle'.format(savedir), measured_param=m_param)
+    # power consumption for PWR_Cycle
+    plot_PWR_Consumption(csv_source_dir=savedir, all_data_types=dataname_list,
+                       output_dir=savedir, powerType='PWR_Cycle')
+    #--------------------------------------------------------------------------------------------------
+
+def run_ASICDAC(inputdir, savedir, temperatures, fembs_to_exclude=[]):
+    for temperature in temperatures:
+        Gains_CALI1(path_to_dataFolder=inputdir, output_dir=savedir, temperature=temperature, withlogs=True)
+        Gains_CALI2(path_to_dataFolder=inputdir, output_dir=savedir, temperature=temperature, withlogs=True)
+        Gains_CALI3_or_CALI4(path_to_dataFolder=inputdir, output_dir=savedir, temperature=temperature, withlogs=True, CALI_number=3)
+        Gains_CALI3_or_CALI4(path_to_dataFolder=inputdir, output_dir=savedir, temperature=temperature, withlogs=True, CALI_number=4)
+        # savegains(path_to_dataFolder=inputdir, output_dir=savedir, temperature=temperature) # <==== FUNCTION TO RUN
+        #
+        # separateCSV_foreachFEMB(path_to_csv='/'.join([savedir, temperature]), output_path='/'.join([savedir, temperature]), datanames=['CALI1', 'CALI3'])
+        separateCSV_foreachFEMB(path_to_csv='/'.join([savedir, temperature]), output_path='/'.join([savedir, temperature]), datanames=['CALI1', 'CALI2', 'CALI3', 'CALI4'])
+        #********************************ENC***************************************************************
+        CALI_numbers = [1, 2, 3, 4]
+        # CALI_numbers = [1,3]
+        for CALI_number in CALI_numbers:
+            get_ENC_CALI(datadir=inputdir, input_dir=savedir, temperature=temperature, CALI_number=CALI_number, fembs_to_exclude=fembs_to_exclude)
+        listgains = ['4_7', '7_8', '14_0', '25_0']
+        larasic_gains = ['{}mVfC'.format(g) for g in listgains]
+        # CALI_numbers = [1, 2, 3, 4]
+        for CALI in CALI_numbers:
+            for larasic_gain in larasic_gains:
+                distribution_ENC_Gain(csv_source_dir=savedir, CALI_number=CALI, temperature=temperature, larasic_gain=larasic_gain, fit=True)
+                distribution_ENC_Gain(csv_source_dir=savedir, CALI_number=CALI, temperature=temperature, larasic_gain=larasic_gain, fit=False)
 
 def run_MON_FE_MON_ADC(inputdir, savedir, temperatures, fembs_to_exclude=[]):
     for T in temperatures:
@@ -26,10 +133,10 @@ def run_MON_FE_MON_ADC(inputdir, savedir, temperatures, fembs_to_exclude=[]):
 
 if __name__ == '__main__':
     #------------------------------------------------------
-    # savedir = '../results/analysis/minisas'
-    # inputdir = '../data'
-    inputdir = 'D:/IO-1865-1C/QC/data'
-    savedir = 'D:/IO-1865-1C/QC/analysis/'
+    savedir = '../results/analysis/test'
+    inputdir = '../data'
+    # inputdir = 'D:/IO-1865-1C/QC/data'
+    # savedir = 'D:/IO-1865-1C/QC/analysis/'
     #------------------------------------------------------
     # measured_info = ['P_meas', 'V_meas', 'I_meas']
     #temperatures = ['LN', 'RT']
@@ -60,8 +167,8 @@ if __name__ == '__main__':
     #    plot_PWR_Cycle(csv_source_dir='../data/analysis/LN/PWR_Cycle', measured_param=m_param)
         # plot_PWR_Cycle(csv_source_dir='D:/IO-1865-1C/QC/analysis/LN/PWR_Cycle', measured_param=m_param)
     # power consumption for PWR_Cycle
-    # plot_PWR_Consumption(csv_source_dir='../data/analysis/', all_data_types=dataname_list,
-    #                    output_dir='../data/analysis', powerType='PWR_Cycle')
+    # plot_PWR_Consumption(csv_source_dir=savedir, all_data_types=dataname_list,
+    #                    output_dir=savedir, powerType='PWR_Cycle')
     # plot_PWR_Consumption(csv_source_dir='D:/IO-1865-1C/QC/analysis/', all_data_types=dataname_list,
     #                     output_dir='D:/IO-1865-1C/QC/analysis', powerType='PWR_Cycle')
     #--------------------------------------------------------------------------------------------------
@@ -111,5 +218,8 @@ if __name__ == '__main__':
     #             distribution_ENC_Gain(csv_source_dir=savedir, CALI_number=CALI, temperature=temperature, larasic_gain=larasic_gain, fit=False)
     
     # savedir = '../results/analysis/test_MON_FE'
-    
-    run_MON_FE_MON_ADC(inputdir=inputdir, savedir=savedir, temperatures=temperatures, fembs_to_exclude=[7, 24, 27, 55, 75])
+    run_PWR_Meas(inputdir=inputdir, savedir=savedir)
+    run_RMS_Pedestal(inputdir=inputdir, savedir=savedir, femb_to_exclude=[])
+    run_ASICDAC(inputdir=inputdir, savedir=savedir, temperatures=temperatures, fembs_to_exclude=[])
+    run_PWR_Cycle(inputdir=inputdir, savedir=savedir)
+    run_MON_FE_MON_ADC(inputdir=inputdir, savedir=savedir, temperatures=temperatures, fembs_to_exclude=[])
